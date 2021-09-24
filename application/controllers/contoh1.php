36543684b2c4556bb81d8a8c75801d9bd9cb3062
aldino17 <rezaaldino22@gmail.com>
2021-09-24 19:32:48 +0700

    Commit pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Reza Aldino
+            saya tinggal di daerah duren sawit
+            hobby saya adalah olahraga";
+            
+    }
+}
